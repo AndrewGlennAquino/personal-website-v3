242e03ca8bb6f69a5e228c2e550e6c6df83849a6
Andrew Aquino <andrewglenn.aquino@gmail.com>
2025-09-17 19:52:45 -0500

    docs

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
+/**
+ * TODO: comment
+ */
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
