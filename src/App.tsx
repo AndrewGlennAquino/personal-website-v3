862c09eac7a2d931261a40930c83f38651a33e0c
Andrew Aquino <andrewglenn.aquino@gmail.com>
2025-09-18 15:57:07 -0500

    fix: remove theme change logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // Library imports
-import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router";
 
 // Component imports
@@ -10,44 +9,25 @@ import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
 import { PageNotFound } from "./pages/PageNotFound";
 
-// Context imports
-import { ThemeContext } from "./contexts/ThemeContext";
-
 /**
  * TODO: comment
  */
 const App = () => {
-  // Hold in state current theme, default is light
-  const [theme, setTheme] = useState<string>("light");
-
-  // Change theme based on toggle
-  useEffect(() => {
-    if (theme === "light") {
-      document.body.classList.add(theme);
-      document.body.classList.remove("dark");
-    } else {
-      document.body.classList.add(theme);
-      document.body.classList.remove("light");
-    }
-  }, [theme]);
-
   return (
     <>
-      <ThemeContext.Provider value={{ theme, setTheme }}>
-        {/* Persistent header */}
-        <Header />
+      {/* Persistent header */}
+      <Header />
 
-        {/* Page routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
+      {/* Page routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-          {/* Route for if page does not exist */}
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        {/* Route for if page does not exist */}
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
 
-        {/* Persistent footer */}
-        <Footer />
-      </ThemeContext.Provider>
+      {/* Persistent footer */}
+      <Footer />
     </>
   );
 };
